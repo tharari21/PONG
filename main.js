c1235666c5b4700ddcb7302d2bcead25bced8152
Tomer Harari <tom@meridianfurnitureusa.com>
2021-01-21 21:59:56 -0500

    Added paddle functionality

--- main.js
+++ main.js
@@ -1,43 +1,58 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
 class Paddle {
     constructor(width, height, color) {
-        this.width = width
-        this.height = height
-        this.color = color
-        this.x = 0
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.x = 10;
         // change this to be half way down the canvas
-        this.y = 0 
+        this.y = (canvas.height/2)-(this.height/2);
+        this.draw();
     }
 
     draw() {
-        
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+
     }
 
     clear() {
-
+        ctx.clearRect(this.x,this.y,this.width,this.height);
     }
 
     move(yOffset) {
-        // dont overstep boundaries
+        const wallOffset = 30;
+        this.clear();
+        this.y += yOffset;
+        if (this.y > (canvas.height-this.height)-wallOffset) {
+            this.y = (canvas.height-this.height)-wallOffset;
+        }
+        else if (this.y < wallOffset) {
+            this.y = wallOffset;
+        }
+        this.draw();
     }
 }
 
 
 class Ball {
     constructor(radius, color, furry=false) {
-        this.radius = radius
-        this.color = color
+        this.radius = radius;
+        this.color = color;
         // inser image of furry ball instead of drawing a circle
-        this.furry = furry
+        this.furry = furry;
 
         // change these values to begin at the center of the canvas
-        this.x = 0
-        this.y = 0
+        this.x = 0;
+        this.y = 0;
 
-        this.xSlope = 2
-        this.ySlope = 1
+        this.xSlope = 2;
+        this.ySlope = 1;
 
         // update 60 frames per second
-        this.velocity = 60
+        this.velocity = 60;
     }
 
     draw() {
@@ -53,3 +68,5 @@ class Ball {
     }
 }
 
+
+const pad = new Paddle(30,100,'blue');
